b87538939849f4eb2588e5b1a358b4b591430cf9
Leg3ndMagician <79544674+Leg3ndMagician@users.noreply.github.com>
2022-04-06 16:40:34 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,47 +1,57 @@
-//let ipAddress = prompt("Please enter a server IP to connect to:");
+/*
+CREDITS:
+
+Pruper - Head Developer
+Broman - Developer
+
+*/
 let connection = new WebSocket('wss://Prupercord.leg3ndmagician.repl.co', "this-is-probably-a-protocol");
 
-let username = "Client " + Math.round(Math.random() * 999999);
+let username = "Client " + Math.floor(Math.random() * 1000000);
 
-connection.onopen = function() {
+connection.onopen = function () {
     log("Connected!");
     usernamePrompt = prompt("Connected to the server. Please enter a username.");
     if (usernamePrompt != "" && usernamePrompt != null) username = usernamePrompt;
     document.getElementById("inputName").value = username;
+
+    connection.send(Message.new("UserJoin", { name: username, newUserCount: null }))
 }
-connection.onmessage = function(msg) {
+connection.onmessage = function (msg) {
     Message.interpret(msg.data.toString());
 }
-connection.onclose = function(e) {
+connection.onclose = function (e) {
     log("Connection has been closed!");
 }
-connection.onerror = function() {
+connection.onerror = function () {
     log("An error occured in your connection!");
 }
 
-establishConnection = function() {
-    if (!ipAddress) {
-        establishConnection();
-    } else {
-        
-    }
-}
-
-window.onload = function() {
-    document.getElementById("sendButton").onclick = function() {
-        let sender = username;
+window.onload = function () {
+    document.getElementById("sendButton").onclick = function () {
         let text = document.getElementById("inputMessage").value;
         if (text == "") return;
-        connection.send(Message.new("Message", {sender: sender, content: text}));
+
+        if (text.startsWith("/")) {
+            // command
+            commandParser(text);
+        } else {
+            // message
+            connection.send(Message.new("Message", { sender: username, content: text, timestamp: Date.now() }));
+        }
         document.getElementById("inputMessage").value = "";
     };
 
-    document.getElementById("inputMessage").addEventListener("keydown", function(event) {
+    document.getElementById("inputMessage").addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             document.getElementById("sendButton").onclick();
         }
     });
 }
+
+window.addEventListener("beforeunload", function (e) {
+    connection.send(Message.new("UserLeave", { name: username, newUserCount: null }))
+});
 
 const Message = {
     new: function (messageType, data) {
@@ -59,51 +69,65 @@ const Message = {
             var message = JSON.parse(toInterpret);
             switch (message.type) {
                 case "Message":
-                    
-                    var date = new Date();
-                    var hours = date.getHours();
-                    var minutes = date.getMinutes();
-                    var daytime = "AM";
-                    if (hours > 12) {
-                        hours -= 12;
-                        daytime = "PM";
-                    }
-                    if (minutes < 10) {
-                        minutes = "0" + minutes;
-                    }
-                    var time = hours + ":" + minutes + " " + daytime;
-                    
-                    
-                    var mess = message.data.content;
-                    while (mess.indexOf(":thumbsup:") > -1) {
-                        mess = mess.substring(0, mess.indexOf(":thumbsup:")) + "👍" + mess.substring(mess.indexOf(":thumbsup:") + 10);
-                    }
-                    while (mess.indexOf(":tm:") > -1) {
-                        mess = mess.substring(0, mess.indexOf(":tm:")) + "™" + mess.substring(mess.indexOf(":tm:") + 4);
-                    }
-                    while (mess.indexOf(":poop:") > -1) {
-                        mess = mess.substring(0, mess.indexOf(":poop:")) + "💩" + mess.substring(mess.indexOf(":poop:") + 6);
-                    }
-                    while (mess.indexOf(":moyai:") > -1) {
-                        mess = mess.substring(0, mess.indexOf(":moyai:")) + "🗿" + mess.substring(mess.indexOf(":moyai:") + 7);
-                    }
-                    if (mess == "/astley" || mess == "/rickroll") {
-                        mess = "<img src=\"https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif\"></img>";
-                    }
-                    
-                    log("[" + message.data.sender + "] (" + time + ") " + mess + "");
+                    var messageContent = parseEmoji(message.data.content);
+                    log("[" + message.data.sender + "] (" + getTimeFor(message.data.timestamp) + ") " + messageContent + "");
+                    break;
+                case "UserJoin":
+                    log("\n" + message.data.name + " has entered the chat (" + message.data.newUserCount + " now online)\n");
+                    break;
+                case "UserLeave":
+                    log("\n" + message.data.name + " has left the chat (" + message.data.newUserCount + " now online)\n");
                     break;
                 default:
                     console.error("Could not interpret message: " + message);
             }
-        } catch(error) {
+        } catch (error) {
             console.log(error);
         }
     }
 }
 
-function log(s) {
+function commandParser(command) {
+    switch (command) {
+        case "/astley":
+        case "/rickroll":
+            connection.send(Message.new("Message", { sender: username, content: "<img src=\"https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif\"></img>" }));
+            break;
+        default:
+            log("\nUnknown command.\n")
+    }
+}
+
+const EmojiList = [
+    { identifier: ":moyai:", emoji: "🗿" },
+    { identifier: ":thumbsup:", emoji: "👍" },
+    { identifier: ":tm:", emoji: "™" },
+    { identifier: ":poop:", emoji: "💩" }
+]
+
+function parseEmoji(text) {
+    var parsedText = text;
+    for (i = 0; i < EmojiList.length; i++) {
+        parsedText = parsedText.replace(EmojiList[i].identifier, EmojiList[i].emoji);
+    }
+}
+
+function getTimeFor(timestamp) {
+    var date = new Date(timestamp);
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var daytime = "AM";
+    if (hours > 12) {
+        hours -= 12;
+        daytime = "PM";
+    }
+
+    var time = hours + ":" + minutes.toString().padStart(2, '0') + daytime;
+    return time;
+}
+
+function log(info) {
     element = document.getElementById('messages')
-    element.innerHTML += "<p>" + s + "</p>";
+    element.innerHTML += "<p>" + info + "</p>";
     element.scrollTop = element.scrollHeight;
 }
